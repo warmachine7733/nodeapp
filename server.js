7dde45ee5bdd834349f6367001b2da5ccb774303
Prateek Jena <prateekjena7733@gmail.com>
2022-11-22 23:19:42 +0530

    added cors globals

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://https://kubernetes.docker.internal:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://kubernetes.docker.internal:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
