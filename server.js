17a66be31d6563993f31800a51fafdc9f0b9ba29
Prateek Jena <prateekjena7733@gmail.com>
2022-11-23 00:10:47 +0530

    removed domain

--- server.js
+++ server.js
@@ -4,27 +4,27 @@ const app = express();
 
 const corsConfig = {
   credentials: true,
-  origin: true,
+  origin: 'https://localhost:3000',
 };
-app.use(cors());
-app.use(function (req, res, next) {
+app.use(cors(corsConfig));
+// app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'https://kubernetes.docker.internal:3000');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Request headers you wish to allow
+//   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 //route
 const port = process.env.PORT || 9000;
 app.get("/", (req, res) =>{
