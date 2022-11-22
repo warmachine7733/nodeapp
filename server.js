6fdab0241c788bf45ee429aea04a93768bcda302
Prateek Jena <prateekjena7733@gmail.com>
2022-11-22 23:36:16 +0530

    samesite none

--- server.js
+++ server.js
@@ -6,29 +6,29 @@ const corsConfig = {
   credentials: true,
   origin: true,
 };
-app.use(cors());
-app.use(function (req, res, next) {
+app.use(cors(corsConfig));
+// app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://kubernetes.docker.internal:3000');
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'https://kubernetes.docker.internal:3000');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Request headers you wish to allow
+//   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 //route
 const port = process.env.PORT || 9000;
 app.get("/", (req, res) =>{
-  res.cookie("testCookie","dumbChrome", { sameSite: 'false', secure: true})
+  res.cookie("testCookie","dumbChrome", { sameSite: 'none', secure: true})
   res.send("deployed in openshift using docker")
 }
 );
