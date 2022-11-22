00516b4c222e8a88a1248e3affcf36445a88c2f9
Prateek Jena <prateekjena7733@gmail.com>
2022-11-22 21:50:36 +0530

    added env for heroku

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+var cors = require('cors');
 const app = express();
-const port = 8080
+
+app.use(cors());
 
 //route
-app.get("/", (req, res) =>
+const port = process.env.PORT;
+app.get("/", (req, res) =>{
+  res.cookie("testCookie","dumbChrome", { sameSite: 'none', secure: true,httpOnly:true})
   res.send("deployed in openshift using docker")
+}
 );
+
 
 //start
 app.listen(port, () => console.log(`node app started in termux on ${port}`));
