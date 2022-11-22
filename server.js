6959c045a09636dbd3e6aa0bb6da53da1c38c93c
Prateek Jena <prateekjena7733@gmail.com>
2022-11-22 23:28:11 +0530

    removed access control allow headers

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
